59fb45a896b490982b58c88528a045adf9b0f5b7
fafbr <fafafebriann11@gmail.com>
2026-01-15 18:09:36 +0700

    revisi

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
-const searchInput = document.getElementById('search-input');
-const cards = document.querySelectorAll('.card');
+const container = document.getElementById('projects-container');
+const seacrhInput = document.getElementById('search-input');
 
-searchInput.addEventListener('input', (e) => {
+let allProjects = [];
+
+async function loadProjects() {
+    try {
+        const response = await fetch('./data.json');
+        allProjects = await response.json();
+
+        displayProjects(allProjects);
+    }
+    catch (error) {
+        console.log('Gagal memuat data project:', error);
+    }
+}
+
+function displayProjects(projects) {
+    container.innerHTML = '';
+
+    projects.forEach(project => {
+        const badgesHTML = project.techStack.map(tech =>
+            `<span class="badge">${tech}</span>`
+        ).join('');
+
+        const cardHTML = 
+            `<article class="card">
+                <div class="badge-container">
+                    ${badgesHTML}
+                </div>
+                <h3 class="card-title">${project.title}</h3>
+                <p class="card-desc">${project.description}</p>
+
+                <div class="card-links">
+                    <a href="${project.liveLink}" target="_blank" class="btn btn-primary">
+                        <i data-feather="external-link"></i> Live Demo
+                    </a>
+
+                    <a href="${project.repoLink}" target="_blank" class="btn btn-secondary">
+                        <i data-feather="github"></i> View Code
+                    </a>
+                </div>
+            </article>
+        `;
+
+        container.innerHTML += cardHTML;
+    });
+    feather.replace();
+}
+
+seacrhInput.addEventListener('input', (e) => {
     const searchString = e.target.value.toLowerCase();
 
-    cards.forEach(card => {
-        const title = card.querySelector('.card-title').innerText.toLowerCase();
-
-        if (title.includes(searchString)) {
-            card.style.display = 'flex';
-        } else {
-            card.style.display = 'none';
-        }
+    const filteredProjects = allProjects.filter(project => {
+        return (
+            project.title.toLowerCase().includes(searchString) || project.description.toLowerCase().includes(searchString)
+        );
     });
+
+    displayProjects(filteredProjects);
 });
+
+loadProjects();
